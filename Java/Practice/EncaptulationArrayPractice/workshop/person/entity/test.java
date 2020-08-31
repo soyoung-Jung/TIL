f09f4048b5cb35c023b11cb6282c9ff4d8b3e9b2
soyoung-Jung <so_young_jung@naver.com>
2020-08-31 23:24:49 +0900

    modify

---
+++ Java/Practice/EncaptulationArrayPractice/workshop/person/entity/test.java
@@ -0,0 +1,32 @@
+package workshop.person.entity;
+
+public class test {
+	private int age;
+	private int num;
+	private String name;
+	private String type;
+	private long testLong;
+	public test() {
+		// TODO Auto-generated constructor stub
+	}
+	public test(int age, int num, String name, String type, long testLong) {
+		super();
+		this.age = age;
+		this.num = num;
+		this.name = name;
+		this.type = type;
+		this.testLong = testLong;
+	}
+	
+	
+	/* 
+	 
+	 ctrl + space :기본생성자 
+	 alt + shift + o : 전체변수를 갖는 전체생성자
+	 alt + shift + s  -> r -> alt + a -> alt+ s -> 엔터 하면 gettersetter전체 만들어짐  
+	 alt + shift + s  -> s -> 엔터 (toString 만들어줌)
+	 DTO 만들때 5초컷 쌉가능
+	 
+	  */
+	
+}
